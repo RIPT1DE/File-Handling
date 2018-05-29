eb114be536eaaaff79ba02e7b4fc6cfa7d33e70e
RIPT1DE <siddiqi.talha1@gmail.com>
2018-05-29 07:05:52 +0500

    Add project files.

---
+++ File Handling/Module1.vb
@@ -0,0 +1,40 @@
+﻿
+Module Module1
+
+    Sub Main()
+        Dim temp As myData
+        For i = 1 To 5
+            temp.data = Console.ReadLine()
+            temp.id = i
+            writeFileRandom(temp)
+        Next
+        Console.WriteLine(readFileRandom(4))
+        Console.ReadLine()
+
+
+        'Dim file1 = New System.IO.StreamReader("test.txt")
+        'file1.ReadLine()
+
+    End Sub
+
+
+    Sub writeFileRandom(ByVal rec As myData)
+        FileOpen(1, "test.bat", OpenMode.Random, ,, Len(New myData))
+        FilePut(1, rec)
+        FileClose(1)
+    End Sub
+
+    Function readFileRandom(ByVal key As Integer)
+        Dim out As myData
+        FileOpen(1, "test.bat", OpenMode.Random,,, Len(New myData))
+        FileGet(1, out, key)
+        Return out
+    End Function
+
+    Structure myData
+        Dim data As String
+        Dim id As Integer
+    End Structure
+
+
+End Module
